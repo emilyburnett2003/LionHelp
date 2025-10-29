f835dfb4120bc4d01a1255ba5ecd2ee3bcced726
Kristine Pham <kristineph101@gmail.com>
2025-10-28 22:20:00 -0400

    add more dummy reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,11 +17,22 @@
 
 # --- Create Reviews ---
 reviews = [
-    { vendor_id: 1, vendor_name: 'John', title: 'Manicure', rating: 5, comment: 'My nails look amazing!' },
-    { vendor_id: 2, vendor_name: 'Kathy', title: 'Meal Swipes', rating: 4, comment: 'Super nice person' },
-    { vendor_id: 1, vendor_name: 'John', title: 'Tutoring', rating: 5, comment: 'The best tutor I’ve ever had.' },
-    { vendor_id: 2, vendor_name: 'Kathy', title: 'Meal Swipes', rating: 1, comment: 'They ghosted me.' },
-    { vendor_id: 3, vendor_name: 'Hannah', title: 'Furniture', rating: 4, comment: 'Personally delivered to me.' }
+    { vendor_id: 1, vendor_name: 'John Doe', title: 'Manicure', rating: 5, comment: 'My nails look amazing!' },
+    { vendor_id: 2, vendor_name: 'Kathy Lee', title: 'Meal Swipes', rating: 4, comment: 'Super nice person' },
+    { vendor_id: 1, vendor_name: 'John Doe', title: 'Tutoring', rating: 5, comment: 'The best tutor I’ve ever had.' },
+    { vendor_id: 2, vendor_name: 'Kathy Lee', title: 'Meal Swipes', rating: 1, comment: 'They ghosted me.' },
+    { vendor_id: 3, vendor_name: 'Hannah Lasso', title: 'Furniture', rating: 4, comment: 'Personally delivered to me.' }
+    { vendor_id: 3, vendor_name: 'Hannah Lasso', title: 'Furniture', rating: 3, comment: 'My chair had a scratch on the back.' },
+    { vendor_id: 3, vendor_name: 'Hannah Lasso', title: 'Furniture', rating: 5, comment: 'Was really friendly.' },
+    { vendor_id: 3, vendor_name: 'Hannah Lasso', title: 'Furniture', rating: 5, comment: 'Sold at a really good price' },
+
+    { vendor_id: 4, vendor_name: 'Barbara Reddington', title: 'Manicure', rating: 1, comment: 'The nail design was not like the picture I showed.' },
+    { vendor_id: 4, vendor_name: 'Barbara Reddington', title: 'Manicure', rating: 2, comment: 'They were late to the appointment.' },
+    { vendor_id: 4, vendor_name: 'Barbara Reddington', title: 'Manicure', rating: 1, comment: 'They were so rude to me.' }
+
+    { vendor_id: 5, vendor_name: 'Janine Abbott', title: 'Dorm Cleaning', rating: 5, comment: 'My room was super clean when I got back.' },
+    { vendor_id: 5, vendor_name: 'Janine Abbott', title: 'Dorm Cleaning', rating: 5, comment: 'She left behind a scented candle that smelled really good.' },
+    { vendor_id: 5, vendor_name: 'Janine Abbott', title: 'Tutoring', rating: 4, comment: 'She was super quick with her services.' }
 ]
 
 reviews.each do |review|
