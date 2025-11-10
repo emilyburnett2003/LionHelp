3f63b0163b115719c724ea11925b79f271c51748
Kristine Pham <kristineph101@gmail.com>
2025-11-10 10:26:31 -0500

    implement login page

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,24 @@
+class AccountsController < ApplicationController
+  def index
+  end
+
+  def create
+    email = account_params[:email]
+    password = account_params[:password]
+
+    user = UserAccount.find_by(email: email)
+    if user.nil?
+      flash[:notice] = "No account found with that email."
+      redirect_to accounts_path
+    elsif user.password != password
+      flash[:notice] = "Incorrect password."
+      redirect_to accounts_path
+    else
+      redirect_to services_path
+    end
+  end
+
+  def account_params
+    params.require(:user_account).permit(:name, :email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :reviews
   get '/vendor_reviews/:vendor_id', to: 'reviews#show_vendor_reviews', as: 'vendor_reviews'
   resources :services
+  resources :accounts
   root 'services#index'
 end
